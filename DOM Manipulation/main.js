4328e320dbd6194d94c9c33d38bb76ae3dcafdac
Nistor Andrei <andreimn97@yahoo.com>
2021-08-06 16:41:52 +0300

    Contact form validation done

---
+++ DOM Manipulation/main.js
@@ -0,0 +1,70 @@
+/*
+<i class="fas fa-check"></i>
+<p>Thank you for contacting us, Vasile</p>
+<span>X</span>
+*/
+
+(function () {
+  const submitButton = document.querySelector('[data-form]');
+  submitButton.addEventListener('submit', handleSubmit);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const firstNameValue = e.target.elements.firstName.value;
+    const lastNameValue = e.target.elements.lastName.value;
+    const gendersValue = e.target.elements.gender.value;
+    const textValue = e.target.elements.message.value;
+    const popupMessage = document.querySelector('.contact-form__message');
+
+    if (firstNameValue.trim() === '' || firstNameValue.trim() === null) {
+      document.querySelector('[data-first-name]').style.border = '1px solid red';
+      return;
+    } else {
+      document.querySelector('[data-first-name]').style.border = '1px solid #000';
+      getSuccesMessage();
+    }
+
+    if (lastNameValue.trim() === '' || lastNameValue.trim() === null) {
+      document.querySelector('[data-last-name]').style.border = '1px solid red';
+      return;
+    } else {
+      document.querySelector('[data-last-name]').style.border = '1px solid #000';
+      getSuccesMessage();
+    }
+
+    if (textValue.trim() === '' || textValue.trim() === null) {
+      document.querySelector('[data-message]').style.border = '1px solid red';
+      return;
+    } else {
+      document.querySelector('[data-message]').style.border = '1px solid #000';
+      getSuccesMessage();
+    }
+
+    if (gendersValue === '') {
+      document.querySelector('[data-genders]').style.border = '1px solid red';
+      return;
+    } else {
+      document.querySelector('[data-genders]').style.border = '1px solid #000';
+      getSuccesMessage();
+    }
+    function getSuccesMessage() {
+      popupMessage.style.display = 'flex';
+      const icon = document.createElement('i');
+      const message = document.createElement('p');
+      const close = document.createElement('span');
+
+      icon.className = 'fas fa-check';
+      message.innerText = `Thank you for contacting us, ${firstNameValue}`;
+      close.innerText = 'X';
+
+      popupMessage.append(icon, message, close);
+
+      console.log(`
+    First Name: ${firstNameValue},
+    Last name : ${lastNameValue},
+    Gender : ${gendersValue},
+    Message : ${textValue}
+    `);
+    }
+  }
+})();
