5efd46f52163e5f7b05aab1879e620a3b3975c2d
Andrei Nistor <andreimn97@yahoo.com>
2021-08-26 16:53:18 +0300

    update bug

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ function largestOfFour(arrays) {
   let biggestNumbers = [];
   let start = 0;
   for (const array of arrays) {
+    start = 0;
     for (let i = 0; i < array.length; i++) {
       if (array[i] > start) {
         start = array[i];
-      } else {
       }
     }
     biggestNumbers.push(start);
@@ -30,9 +30,9 @@ function largestOfFour(arrays) {
 
 console.log(
   largestOfFour([
-    [17, 23, 25, 12],
-    [25, 7, 34, 48],
-    [4, -10, 18, 21],
-    [-72, -3, -17, -10],
+    [13, 27, 18, 26],
+    [4, 5, 1, 3],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
   ])
 );
